463e0b17e2949731c4759353e506a28b750dafef
Karima KHIRELLAH <karima.khirellah@viseo.com>
2016-10-08 21:01:40 +0100

    add and delete operations

--- www/js/app.js
+++ www/js/app.js
@@ -5,88 +5,56 @@
 // the 2nd parameter is an array of 'requires'
 angular
 .module('todo', [
-  'ionic',
-  'LocalStorageModule'
+  'ionic'
   ])
 
-.config(function (localStorageServiceProvider) {
-  localStorageServiceProvider
-    .setPrefix('todo');
-})
-
-.controller('main', function ($scope, localStorageService) { 
-  $scope.todo = "";
+.controller('main', function ($scope) { 
   $scope.empty = false;
-  $scope.exist = false;
+  $scope.duplicated = false;
   $scope.todos = [
-    {
-       name: "todo 1"
-    },
-    {
-       name: "todo 2"
-    },
-    {
-       name: "todo 3"
-    },
-    {
-       name: "todo 4"
-    }];
+    { name: "todo 1"},
+    { name: "todo 2"},
+    { name: "todo 3"},
+    { name: "todo 4"}
+  ];
 
-    $scope.addTodo = addTodo;
-    $scope.deleteTodo = deleteTodo;
+  $scope.addTodo = addTodo;
+  $scope.deleteTodo = deleteTodo;
+  $scope.isEmpty = isEmpty;
+  $scope.isDuplicated = isDuplicated;
 
-    function addTodo(todo) {
-      if (!checkExist(todo) && checkNotEmpty(todo)) {
-        $scope.todos.push({name: todo});
-        $scope.exist = false;
-        $scope.empty = false;
-      } else if (checkExist(todo)) {
-        $scope.exist = true;
-      } else if (!checkNotEmpty(todo)) {
-        $scope.empty = true;
+  function addTodo (todo) {
+    if (!isEmpty(todo) && !isDuplicated(todo)) {
+      $scope.todos.push({
+        name : todo
+      });
+      $scope.empty = false;
+      $scope.duplicated = false;
+      console.log("test");
+    } else if (isEmpty(todo)) {
+      $scope.empty = true;
+    } else if (isDuplicated(todo)) {
+      $scope.duplicated = true;
+    } 
+  }
+
+  function deleteTodo (todo) {
+    $scope.todos = $scope.todos.filter(function(obj) {
+      return todo.name !== obj.name;
+    })
+  }
+
+  function isEmpty (todo) {
+    return todo === undefined || todo.length === 0;
+  }
+
+  function isDuplicated (todo) {
+    for (var i=0; i < $scope.todos.length ; i++) {
+      if ($scope.todos[i].name === todo) {
+        return true;
       }
     }
-
-    function checkNotEmpty (name) {
-      return name.length !== 0;
-    }
-
-    function checkExist (name) {
-      for (var i = 0; i < $scope.todos.length; i++) {
-        if ($scope.todos[i].name === name) {
-          return true
-        }
-      }
-      return false;
-    }
-
-    function deleteTodo (todo) {
-      // body...
-      $scope.todos = $scope.todos.filter(function(obj) {
-        return obj.name !== todo.name;
-      })
-    }
+    return false;
+  }
 
 });
-
-
-
-
-
-/*.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})*/
